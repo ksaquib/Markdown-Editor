163cf827a93c5b8daf3e40684eefd8ed97b51a2b
Saquib Khan <ksaquib54@gmail.com>
2021-02-25 16:21:27 +0530

    Added react markdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ReactMarkdown from "react-markdown";
 
 export default function App() {
+  const [markdown, setMarkdown] = useState("# supp");
+
+  const handleChange = ({ target: { value } }) => {
+    setMarkdown(value);
+  };
   return (
     <div className="app">
-      <textarea />
+      <textarea value={markdown} onChange={handleChange} />
 
-      <div className="preview" />
+      {/* <div
+        className="preview"
+        dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+      /> */}
+      <ReactMarkdown className="preview" source={markdown} />
     </div>
   );
 }
